5300a97a71a8c8f6497ae13a82c593050bd41aa3
Olayinka Ekundayo <olayinkaekundayo2@gmail.com>
2026-04-11 01:25:46 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -11,6 +11,16 @@ WARNING:
     you have proper backup before running this script
 
 */
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 
 -- Create Database
 Create database Datawarehouse;
